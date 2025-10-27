330ed2d270fe4e0dd5e75a53a081967b72923271
Matthew Perryman <matthew_perryman@outlook.com>
2025-10-27 22:54:32 +0000

    Trying to read camera controls instead of camera_metadata

--- server_package/api/camera_controller.py
+++ server_package/api/camera_controller.py
@@ -37,7 +37,7 @@ class ImageStream:
 		if focus_mode == "Continuous":
 			self.picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
 
-			if self.picam2.capture_metadata()['AfMode'] != controls.AfModeEnum.Continuous:
+			if self.picam2.camera_controls()['AfMode'] != controls.AfModeEnum.Continuous:
 				logging.write_log("server", "Warning: Continuous focus mode not set correctly")
 			else:
 				logging.write_log("server", "Focus mode set to Continuous")
@@ -45,12 +45,12 @@ class ImageStream:
 			self.picam2.set_controls({"AfMode": controls.AfModeEnum.Manual})
 			self.picam2.set_controls({"LensPosition": focus_value})
 
-			if self.picam2.capture_metadata()['AfMode'] != controls.AfModeEnum.Manual:
+			if self.picam2.camera_controls()['AfMode'] != controls.AfModeEnum.Manual:
 				logging.write_log("server", "Warning: Manual focus mode not set correctly")
 			else:
 				logging.write_log("server", "Focus mode set to Manual")
 				
-			if self.picam2.capture_metadata()['LensPosition'] != focus_value:
+			if self.picam2.camera_controls()['LensPosition'] != focus_value:
 				logging.write_log("server", "Warning: Manual focus value not set correctly")
 			else:
 				logging.write_log("server", f"Focus value set to {focus_value}")
@@ -58,7 +58,7 @@ class ImageStream:
 			logging.write_log("server", "Warning: Focus mode not recognized")
 		
 		print("Focus Mode Set to ", focus_mode
-			  , " with value ", self.picam2.capture_metadata()['LensPosition'])
+			  , " with value ", self.picam2.camera_controls()['LensPosition'])
 
 	# Get the focus mode
 	def get_focus_mode(self):
