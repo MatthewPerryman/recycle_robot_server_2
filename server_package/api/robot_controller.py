5b3b019572cc1eb1ca0e1664c1ac5cb41069a961
Matthew Perryman <matthew_perryman@outlook.com>
2025-10-28 23:40:14 +0000

    Trying a slower speed for accuracy

--- server_package/api/robot_controller.py
+++ server_package/api/robot_controller.py
@@ -86,7 +86,7 @@ class RobotController:
 			self.swift.set_polar(stretch=new_location[0],
 								rotation=new_location[1],
 								height=new_location[2],
-								speed=speed)
+								speed=10)
 			return True
 
 	# Reset robot location
